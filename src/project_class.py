58a3f34a1a082535e86ba3a2ceaf3432a2e1604f
xziste <17alexdmz99@gmail.com>
2025-05-04 22:36:35 +0300

    update and fixes class in project_class.py

--- src/project_class.py
+++ src/project_class.py
@@ -19,8 +19,13 @@ class Category:
     description = str
     products = list
 
+    category_count = 0
+    product_count = 0
+
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
         self.products = products
+        Category.category_count += 1
+        Category.product_count += len(products)
