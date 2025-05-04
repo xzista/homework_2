b56accd097094d46e72ffafba46ddca37de2f03c
xziste <17alexdmz99@gmail.com>
2025-05-04 23:03:24 +0300

    update fixtures and add test class Product and class Category

--- tests/test_project_class.py
+++ tests/test_project_class.py
@@ -4,12 +4,27 @@ from src.project_class import Product, Category
 
 
 @pytest.fixture
-def product1():
-    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+def products():
+    return [Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)]
 
 
 @pytest.fixture
-def category1():
+def category1(products):
     return Category("Смартфоны",
                     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                    [product1, product2, product3])
+                    products=products)
+
+
+def test_init_products(products):
+    assert products[0].name == "Samsung Galaxy S23 Ultra"
+    assert products[1].description == "512GB, Gray space"
+    assert products[2].price == 31000.0
+    assert products[0].quantity == 5
+
+
+def test_init_categories(category1):
+    assert category1.name == "Смартфоны"
+    assert category1.category_count == 1
+    assert category1.product_count == 3
