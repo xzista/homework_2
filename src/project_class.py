eef8ceea137475b12e837e1867894283c21e6e24
xziste <17alexdmz99@gmail.com>
2025-05-08 05:27:01 +0300

    update class Category and add method add_product()

--- src/project_class.py
+++ src/project_class.py
@@ -26,6 +26,10 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, new_products):
+        self.__products.append(new_products)
