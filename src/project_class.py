bd75f20e3cb8b1ad1a329d822d249a395e64cb84
xziste <17alexdmz99@gmail.com>
2025-05-15 19:35:36 +0300

    add dunder method __add__ in class Product

--- src/project_class.py
+++ src/project_class.py
@@ -11,6 +11,10 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
 
+
+    def __add__(self, other):
+        return self.price * self.quantity + other.price * other.quantity
+
     @classmethod
     def new_product(cls, dict_prod, list_of_prod=None):
         name = dict_prod.get("name")
