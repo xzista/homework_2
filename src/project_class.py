1dc75214968e5bc18afafb85569925cf1a53a9c4
xziste <17alexdmz99@gmail.com>
2025-05-16 03:24:21 +0300

    add new class ProductIterator and new getter: products in class Category

--- src/project_class.py
+++ src/project_class.py
@@ -77,3 +77,27 @@ class Category:
     def see_products(self):
         list_products = [str(product) for product in self.__products]
         return "\n".join(list_products)
+
+    @property
+    def products(self):
+        return self.__products
+
+
+class ProductIterator:
+    """Вспомогательный класс для перебора товаров одной категории"""
+    def __init__(self, category: Category):
+        self.category = category
+        self.index = 0
+
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+        if self.index < len(self.category.products):
+            product = self.category.products[self.index]
+            self.index += 1
+            return product
+        else:
+            raise StopIteration
