6e3daa508808dacc23d396a49ffa7b253160c966
xziste <17alexdmz99@gmail.com>
2025-05-24 17:24:05 +0300

    add conftest.py and moved fixtures

--- tests/test_project_class.py
+++ tests/test_project_class.py
@@ -2,41 +2,7 @@ from unittest.mock import patch
 
 import pytest
 
-from src.category import ProductIterator, Category
 from src.product import Product
-
-
-@pytest.fixture
-def products():
-    return [
-        Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5),
-        Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-        Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
-    ]
-
-
-@pytest.fixture
-def dict_product():
-    return {
-        "name": "Samsung Galaxy S23 Ultra",
-        "description": "256GB, Серый цвет, 200MP камера",
-        "price": 180001.0,
-        "quantity": 10,
-    }
-
-
-@pytest.fixture
-def category1(products):
-    return Category(
-        "Смартфоны",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        products=products,
-    )
-
-
-@pytest.fixture
-def product_iter(category1):
-    return ProductIterator(category1)
 
 
 def test_init_products(products):
