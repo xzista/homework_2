1e5dbc84d03be503fb49753616964ecdbe2a3800
xziste <17alexdmz99@gmail.com>
2025-05-24 17:49:00 +0300

    divided tests for Product and Category on modules test_product.py and test_category.py

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,39 +1,12 @@
-from unittest.mock import patch
-
 import pytest
 
 from src.product import Product
-
-
-def test_init_products(products):
-    assert products[0].name == "Samsung Galaxy S23 Ultra"
-    assert products[1].description == "512GB, Gray space"
-    assert products[2].price == 31000.0
-    assert products[0].quantity == 5
 
 
 def test_init_categories(category1):
     assert category1.name == "Смартфоны"
     assert category1.category_count == 1
     assert category1.product_count == 3
-
-
-def test_new_product(dict_product, products):
-    assert products[0].new_product(dict_product, products).price == 180001.0
-    assert products[0].quantity == 15
-
-
-def test_get_price(capsys, products):
-    products[1].price = -1
-    captured = capsys.readouterr()
-    assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
-    with patch("builtins.input", return_value="y"):
-        products[2].price = 1
-        assert products[2].price == 1
-    with patch("builtins.input", return_value="n"):
-        products[0].price = 100
-        captured = capsys.readouterr()
-        assert "Отмена изменения цены" in captured.out
 
 
 def test_add_product(category1, products):
@@ -47,19 +20,13 @@ def test_see_products(category1):
     assert category1.see_products.strip() == expected.strip()
 
 
-def test_str_products(capsys, products):
-    print(str(products[1]))
-    captured = capsys.readouterr()
-    assert "Iphone 15, 210000.0 руб. Остаток: 8 шт." in captured.out
-
-
 def test_str_category(capsys, category1):
     print(str(category1))
     captured = capsys.readouterr()
     assert "Смартфоны, количество продуктов: 13 шт." in captured.out
 
 
-def test_products(products, category1):
+def test_product(products, category1):
     assert products[0] == category1.product[0]
 
 
---
+++ tests/test_product.py
@@ -0,0 +1,32 @@
+from unittest.mock import patch
+
+
+def test_init_products(products):
+    assert products[0].name == "Samsung Galaxy S23 Ultra"
+    assert products[1].description == "512GB, Gray space"
+    assert products[2].price == 31000.0
+    assert products[0].quantity == 5
+
+
+def test_new_product(dict_product, products):
+    assert products[0].new_product(dict_product, products).price == 180001.0
+    assert products[0].quantity == 15
+
+
+def test_get_price(capsys, products):
+    products[1].price = -1
+    captured = capsys.readouterr()
+    assert captured.out == "Цена не должна быть нулевая или отрицательная\n"
+    with patch("builtins.input", return_value="y"):
+        products[2].price = 1
+        assert products[2].price == 1
+    with patch("builtins.input", return_value="n"):
+        products[0].price = 100
+        captured = capsys.readouterr()
+        assert "Отмена изменения цены" in captured.out
+
+
+def test_str_products(capsys, products):
+    print(str(products[1]))
+    captured = capsys.readouterr()
+    assert "Iphone 15, 210000.0 руб. Остаток: 8 шт." in captured.out
