36438d282572fa4b1e51f4da5cc977ad1006c9b2
xziste <17alexdmz99@gmail.com>
2025-05-24 18:01:28 +0300

    add new fixtures in conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import pytest
 
 from src.category import Category, ProductIterator
+from src.lawn_grass import LawnGrass
 from src.product import Product
+from src.smartphone import Smartphone
 
 
 @pytest.fixture
@@ -35,3 +37,16 @@ def category1(products):
 @pytest.fixture
 def product_iter(category1):
     return ProductIterator(category1)
+
+
+@pytest.fixture
+def smartphones():
+    return [Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"),
+            Smartphone("Iphone 15", "512GB, Gray space", 210000.0, 8, 98.2, "15", 512, "Gray space"),
+            Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")]
+
+
+@pytest.fixture
+def lawn_grasses():
+    return [LawnGrass("Газонная трава", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый"),
+            LawnGrass("Газонная трава 2", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")]
