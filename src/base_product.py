5b52cc1d6e4dedc1bf4131c66f0bffeebdf76795
xziste <17alexdmz99@gmail.com>
2025-06-03 01:42:12 +0300

    update class BaseProduct in base_product.py

--- src/base_product.py
+++ src/base_product.py
@@ -14,11 +14,3 @@ class BaseProduct(ABC):
     @abstractmethod
     def __add__(self, other):
         pass
-
-    @abstractmethod
-    def new_product(self, dict_prod, list_of_prod=None):
-        pass
-
-    @abstractmethod
-    def price(self):
-        pass
