c9a278406d352e1c2c31e526e8484f9ea1b89003
xziste <17alexdmz99@gmail.com>
2025-06-03 01:41:33 +0300

    update class Product in product.py

--- src/product.py
+++ src/product.py
@@ -1,4 +1,8 @@
-class Product:
+from src.base_product import BaseProduct
+from src.mixins import MixinLog
+
+
+class Product(MixinLog ,BaseProduct):
     """Класс для продуктов"""
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
@@ -6,6 +10,10 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self.price}, {self.quantity})"
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
