dd8174ea080a657c9838217a0748e12ae3c01bfd
xziste <17alexdmz99@gmail.com>
2025-06-03 16:44:08 +0300

    add new class CategoryItems(ABC) in category_items.py

---
+++ src/category_items.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class CategoryItems(ABC):
+    """Абстрактный класс для классов Category и Order"""
+    @abstractmethod
+    def __init__(self):
+        self.products = []
+
+    @abstractmethod
+    def __str__(self):
+        pass  # pragma: no cover
