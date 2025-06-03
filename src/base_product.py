3314bb86c89a71eb03cffc3d493b5493855df48a
xziste <17alexdmz99@gmail.com>
2025-06-03 15:07:55 +0300

    update class BaseProduct

--- src/base_product.py
+++ src/base_product.py
@@ -4,8 +4,11 @@ from abc import ABC, abstractmethod
 class BaseProduct(ABC):
 
     @abstractmethod
-    def __init__(self):
-        pass
+    def __init__(self, name: str, description: str, price: float, quantity: int):
+        self.name = name
+        self.description = description
+        self.__price = price
+        self.quantity = quantity
 
     @abstractmethod
     def __str__(self):
