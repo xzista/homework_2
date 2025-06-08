1636ef98d4653d027d9917baad8393f83ea68352
xziste <17alexdmz99@gmail.com>
2025-06-08 16:00:29 +0300

    add new exception class ZeroQuantityError in zero_quantity_exception.py

---
+++ src/zero_quantity_exception.py
@@ -0,0 +1,9 @@
+class ZeroQuantityError(Exception):
+    """Класс исключения, который отвечает за обработку добавления товара с нулевым количеством."""
+
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Попытка добавления товара с нулевым количеством'
+
+
+    def __str__(self):
+        return self.message
